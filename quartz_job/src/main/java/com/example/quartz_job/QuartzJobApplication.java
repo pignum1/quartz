59eacf45802e07abd353ad0e59bc6af9938a87be
魏星阳 <827928660@qq.com>
2020-04-14 10:51:34 +0800

    自动添加创建时间、修改时间

--- quartz_job/src/main/java/com/example/quartz_job/QuartzJobApplication.java
+++ quartz_job/src/main/java/com/example/quartz_job/QuartzJobApplication.java
@@ -2,8 +2,10 @@ package com.example.quartz_job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QuartzJobApplication {
 
     public static void main(String[] args) {
--- quartz_job/src/main/java/com/example/quartz_job/controller/ScheduleTimeCotroller.java
+++ quartz_job/src/main/java/com/example/quartz_job/controller/ScheduleTimeCotroller.java
@@ -1,5 +1,10 @@
 package com.example.quartz_job.controller;
+import java.util.Date;
 
+import com.example.quartz_job.dao.ScheduleTimeDao;
+import com.example.quartz_job.entity.ScheduleTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2020/4/13 11:13
  * @Version: 1.0
  */
-@RestController("/scheduleTime")
+@RestController
 public class ScheduleTimeCotroller {
+    @Autowired
+    private ScheduleTimeDao scheduleTimeDao;
+
+    @PostMapping("/addTest")
+    public void addTest(){
+        ScheduleTime scheduleTime = new ScheduleTime();
+        scheduleTime.setJobName("123");
+        scheduleTime.setBeginTime("123");
+        scheduleTime.setEndTime("123");
+        scheduleTime.setResult("123");
+        scheduleTime.setStatus("123");
+        scheduleTime.setDeleteFlag(false);
+        scheduleTimeDao.save(scheduleTime);
+
+    }
 }
